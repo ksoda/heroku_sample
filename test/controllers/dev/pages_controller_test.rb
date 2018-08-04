d480e18f4a1ee6029be723a98ef3ad9560d7bb83
Ken Sonoda <ken.soda0@gmail.com>
2018-08-04 22:31:45 +0900

    Remove deprecations

--- test/controllers/dev/pages_controller_test.rb
+++ test/controllers/dev/pages_controller_test.rb
@@ -5,6 +5,6 @@ require 'test_helper'
 describe Dev::PagesController do
   it 'should get home' do
     get dev_pages_home_url
-    value(response).must_be :success?
+    value(response).must_be :successful?
   end
 end
