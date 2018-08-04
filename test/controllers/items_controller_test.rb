785a116e3d4a36fbb540c586c9dae18ed8ed5c23
Ken Sonoda <ken.soda0@gmail.com>
2018-08-05 06:50:55 +0900

    Make drafts

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 describe ItemsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe 'GET /todos/:todo_id/items' do
+    it 'returns all todo items when todo exists'
+  end
+  describe 'POST /todos/:todo_id/items' do
+    it 'returns status code 201 when request attributes are valid'
+  end
+  describe 'GET /todos/:todo_id/items/:id' do
+    it 'returns the item when todo item exists'
+  end
 end
