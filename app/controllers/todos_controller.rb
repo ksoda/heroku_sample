4a80feeedfd7e220c230d05ed71574e872c767b5
Ken Sonoda <ken.soda0@gmail.com>
2018-08-05 02:23:02 +0900

    POST todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,6 +3,21 @@
 class TodosController < ApplicationController
   def index
     todos = Todo.take(3)
-    render json: todos
+    render json: todos, status: :ok
+  end
+
+  def create
+    todo = Todo.create!(todo_params)
+    render json: todo, status: :created
+  end
+
+  private
+
+  def todo_params
+    params.permit(:title, :created_by)
+  end
+
+  def find_todo
+    Todo.find(params[:id])
   end
 end
--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -3,8 +3,24 @@
 require 'test_helper'
 
 describe TodosController do
-  it 'must be a real test' do
-    get '/todos'
-    value(response).must_be :successful?
+  def json
+    JSON.parse(response.body)
+  end
+  describe 'GET /todos' do
+    it 'returns todos' do
+      get '/todos'
+      value(response).must_be :successful?
+    end
+  end
+
+  it 'returns todos' do
+    size = lambda {
+      get '/todos'
+      json.size
+    }
+
+    before_size = size.call
+    post '/todos', params: { title: 'title', created_by: 'John' }
+    size.call.wont_be :eql?, before_size
   end
 end
