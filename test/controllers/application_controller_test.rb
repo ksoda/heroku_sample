6f1437ba00bfd24133ceea15d6c74453c4447f66
Ken Sonoda <ken.soda0@gmail.com>
2018-08-13 00:07:16 +0900

    Add tests

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -3,10 +3,36 @@
 require 'test_helper'
 
 describe ApplicationController do
+  include TestSupport
   let(:user) { users(:john) }
+  before do
+    class ::TestingsController < ApplicationController
+      def index
+        head :no_content
+      end
+    end
+
+    Rails.application.configure do
+      routes.disable_clear_and_finalize = true
+      routes.draw do
+        resources 'testings', only: 'index'
+      end
+      routes.disable_clear_and_finalize = false
+    end
+  end
+
+  after do
+    Object.send(:remove_const, :TestingsController)
+  end
 
   describe '#authorize_request' do
-    it 'sets the current user when auth token is passed'
-    it 'raises MissingToken error when auth token is not passed'
+    it 'fails when auth token is not passed' do
+      get '/testings'
+      value(response).must_be :unprocessable?
+    end
+    it 'sets the current user when auth token is passed' do
+      get '/testings', headers: { 'Authorization' => token_generator(user.id) }
+      value(response).must_be :successful?
+    end
   end
 end
