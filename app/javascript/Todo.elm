a7db66440c95a0fc1b8d89c16fc05cad8fe78d3c
Ken Sonoda <ken.soda0@gmail.com>
2018-08-19 00:53:37 +0900

    Render a todo list

--- app/javascript/Todo.elm
+++ app/javascript/Todo.elm
@@ -1,17 +1,75 @@
 module Main exposing (main)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Html.Lazy exposing (lazy, lazy2)
 import Debug exposing (crash)
 
-main = view
 
-view =
-  div []
-  [ input
-    [ type_ "text"] []
-    , button [] [text "Submit"]
-    , button [] [text "Remove All"]
-    , div [] []
-  ]
+main =
+    beginnerProgram
+        { model = model
+        , update = update
+        , view = view
+        }
+
+
+type alias Model =
+    { todo : String
+    , todos : List String
+    }
+
+
+model : Model
+model =
+    { todo = ""
+    , todos = []
+    }
+
+
+type Msg
+    = UpdateTodo String
+    | AddTodo
+    | RemoveAll
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UpdateTodo text ->
+            { model | todo = text }
+
+        AddTodo ->
+            { model | todos = model.todo :: model.todos }
+
+        RemoveAll ->
+            { model | todos = [] }
+
+
+todoItem : String -> Html Msg
+todoItem todo =
+    li [] [text todo, button [] [text "x"]]
+
+
+todoList : List String -> Html Msg
+todoList todos =
+    let
+        child =
+            List.map todoItem todos
+    in
+        ul [] child
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input
+            [ type_ "text"
+            , onInput UpdateTodo
+            , value model.todo
+            ]
+            []
+        , button [ onClick AddTodo ] [ text "Submit" ]
+        , button [ onClick RemoveAll ] [ text "Remove All" ]
+        , div [] [ todoList model.todos ]
+        ]
