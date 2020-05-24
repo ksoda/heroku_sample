5021552ae98dd117f3e10bc75bfaab4f64d56bff
Ken Sonoda <ksoda@users.noreply.github.com>
2020-05-25 00:17:43 +0900

    perf: speed up for modern browsers

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = (env, argv) => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "index.ejs",
+        scriptLoading: "defer",
         templateParameters: {
           service: JSON.stringify(
             argv.mode === "production"
