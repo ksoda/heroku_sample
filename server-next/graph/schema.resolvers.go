23c9a13bd13c361ab45f7dde217351c93e439dfd
Ken Sonoda <ksoda@users.noreply.github.com>
2020-08-10 00:41:31 +0900

    add deps

--- server-next/graph/schema.resolvers.go
+++ server-next/graph/schema.resolvers.go
@@ -5,6 +5,7 @@ package graph
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
 
 	"github.com/ksoda/todo-app/graph/generated"
@@ -12,11 +13,17 @@ import (
 )
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   fmt.Sprintf("T%d", rand.Int()),
+		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.todos, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
